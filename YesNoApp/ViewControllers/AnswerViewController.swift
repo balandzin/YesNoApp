e3838e3e42e81c3b355b37a8071a79bd8dd6cc25
balandzin <balandzin2602@gmail.com>
2024-02-20 08:55:22 +0300

    Добавил язык

--- YesNoApp/ViewControllers/AnswerViewController.swift
+++ YesNoApp/ViewControllers/AnswerViewController.swift
@@ -11,22 +11,41 @@ final class AnswerViewController: UIViewController {
     
     @IBOutlet var answerLabel: UILabel!
     @IBOutlet var imageView: UIImageView!
+    @IBOutlet var languageSegmentedControl: UISegmentedControl!
+    @IBOutlet var answerButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setValues()
+        
         answerLabel.text = ""
     }
-
-
+    
+    
     @IBAction func answerButtonAction() {
-        let answer = Bool.random()
-        
         answerLabel.text = ""
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            self.answerLabel.text = answer ? "Yes" : "No"
-            self.imageView.image = answer ? UIImage(named: "Yes") : UIImage(named: "No")
+            self.setValues()
         }
+    }
+    
+    @IBAction func LanguageSCAction() {
+        setValues()
+    }
+    
+    private func setValues() {
+        let answer = Bool.random()
+
+        if languageSegmentedControl.selectedSegmentIndex == 1 {
+            answerLabel.text = answer ? "Yes" : "No"
+            answerButton.setTitle("Get answer", for: .normal)
+
+        } else {
+            answerLabel.text = answer ? "Да" : "Нет"
+            answerButton.setTitle("Получить ответ", for: .normal)
+        }
+        imageView.image = answer ? UIImage(named: "Yes") : UIImage(named: "No")
     }
 }
